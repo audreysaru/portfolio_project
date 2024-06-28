5244428b4d7d0f3aa9707f7ea1ae86453eb02efb
audreysaru <saruaudrey@gmail.com>
2024-06-28 05:37:52 +0300

    Resolve build deploy error

--- Daily3/frontend/src/pages/Homepage.js
+++ Daily3/frontend/src/pages/Homepage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import jwtDecode from 'jwt-decode';
 import { getProfile, addActivityHistory } from '../api';
 import Movement from './Movement';
 import Meditation from './Meditation';
