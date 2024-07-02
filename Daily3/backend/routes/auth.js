158d9e363c0e768281d2cb4869de08042618e28a
audreysaru <saruaudrey@gmail.com>
2024-07-02 22:45:32 +0300

    Resolve backend build error

--- Daily3/backend/routes/auth.js
+++ Daily3/backend/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
+const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
@@ -9,7 +9,7 @@ router.post('/signup', async (req, res) => {
     const { firstName, lastName, username, email, password, birthday } = req.body;
 
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await argon2.hash(password, 10);
         const user = await User.create({ firstName, lastName, username, email, password: hashedPassword, birthday });
 
         res.status(201).json(user);
@@ -23,7 +23,7 @@ router.post('/login', async (req, res) => {
 
     try {
         const user = await User.findOne({ where: { email } });
-        if (!user || !await bcrypt.compare(password, user.password)) {
+        if (!user || !await argon2.compare(password, user.password)) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
